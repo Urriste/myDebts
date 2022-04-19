3484e68d7f0a809a9700be41d843faf66280a4ef
Lucas Urriste <lurriste@mobydigital.com>
2022-04-19 16:11:13 -0300

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AddDebtComponent } from './components/add-debt/add-debt.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AddDebtComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
